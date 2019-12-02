78ef735131cb21557a670859b4d1691093464137
qiyuan98 <447283629@qq.com>
2019-12-02 22:45:44 +0800

    make a few modification

--- R/d_stat.R
+++ R/d_stat.R
@@ -1,19 +1,27 @@
-d_stat <- function(ssize = ssize,g = group,index,distance){
-  total.s <- ssize * g
+#' calculate the energy statistics from balanced designed samples
+#'
+#' This fucntion calculate the statistics to test multrivariate test for equal distributions based on energy method
+#' @param size the sample size
+#' @param group the number of the groups of the samples
+#' @param index for permutation test, indicate how to divide the samples into groups
+#' @param distance the distance matrix
+#' @return energy statistics used for multivariate test for equal distribution
+d_stat <- function(size,group,index,distance){
+  total <- size * group
   ind <- index;ind <- matrix(ind,byrow = TRUE,nrow = group)
-  add.w <- numeric(g)
-  for (i in 1:g) {
-    add.w[i] <- ssize/2 * mean(distance[ind[i,],ind[i,]])
+  add.w <- numeric(group)
+  for (i in 1:group) {
+    add.w[i] <- size/2 * mean(distance[ind[i,],ind[i,]])
   }
   w <- sum(add.w)
-  s <- 0
-  for(i in 1:(g-1)){
-    for(j in (i+1):g){
-      s = s + ssize^2/total.size *
+  s <- numeric(1)
+  for(i in 1:(group-1)){
+    for(j in (i+1):group){
+      s = s + size^2/total *
         ( 2*mean(distance[ind[i,],ind[j,]]) -
             mean(distance[ind[j,],ind[j,]]) -
             mean(distance[ind[i,],ind[i,]]) )
     }
   }
-  s/(g - 1)/(w/(total.s - g))
+  s/(group - 1)/(w/(total - group))
 }
---
+++ R/equal_test.R
@@ -0,0 +1,21 @@
+#'multivariate test for equal distribution
+#'
+#'implement a permuation test, using energy method to calculate method for multivariate test for equal distribution
+#'@dat data should be a matrix with each row representing a point
+#'@size the sample size
+#'@group the number of the groups of the samples
+#'@dim the dimension of the point
+#'@return p value from the permutation test
+equal_test <- function(dat,size,group,dim,R = 199){
+  total.size <- size*group
+  distance <- as.matrix(dist(dat,diag = TRUE,upper = TRUE,
+                             method = "minkowski",p = 1))
+  d_stat_storage <- numeric(R+1)
+  index <- 1:(size*group)
+  d_stat_storage[1] <- d_stat(size,group,index,distance)
+  for(k in 1:R){
+    index <- sample(1:total.size,total.size,replace = FALSE)
+    d_stat_storage[k+1] <- d_stat(size,group,index,distance)
+  }
+  mean(d_stat_storage > d_stat_storage[1])
+}
